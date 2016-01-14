e967af9fd1b9c6d375dda8ba3e47b2edeb3e94af
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-01-14 15:38:41 -0600

    Stuff

--- server/main_test.go
+++ server/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"net/http"
 	"testing"
 
 	"gopkg.in/mgo.v2/bson"
@@ -52,7 +54,7 @@ func TestDatabase(t *testing.T) {
 	if err != nil {
 		t.Errorf("Failed to find test users in the database\n%v\n", err)
 	}
-	t.Log("  result:\n", result)
+	// t.Log("  result:\n", result)
 
 	info, err := collection.RemoveAll(q)
 	if err != nil {
@@ -62,4 +64,36 @@ func TestDatabase(t *testing.T) {
 	if info.Removed < 2 {
 		t.Error("Failed to either add or remove test users from the database")
 	}
+}
+
+func TestBasicServer(t *testing.T) {
+	t.Log("Testing basic server capabilities")
+	url := "http://localhost:8020/"
+	res, err := http.Get(url)
+	defer res.Body.Close()
+	if err != nil {
+		t.Errorf("Failed to get information from the server\n%v\n", err)
+	} else if res.StatusCode != 200 {
+		t.Errorf("Server returned status code %v\n", res.StatusCode)
+	}
+}
+
+func TestSignup(t *testing.T) {
+	t.Log("Testing signup")
+	url := "http://localhost:8020/api/users/signup"
+
+	var jsonStr = []byte(`{"Username":"Bob","Firstname": "Bob","Password": "Sue"}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	if err != nil {
+		t.Errorf("Failed to signup Bob\n%v\n", err)
+	} else if res.StatusCode != 201 {
+		t.Errorf("Expected status code 201, got %v\n", res.StatusCode)
+	}
+
 }
