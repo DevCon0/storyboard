ecad81bb0e6b0089f8aee4b01e765390c4276625
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-01-14 13:26:24 -0600

    Implemented signup on server side

--- server/handler.go
+++ server/handler.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 
+	"golang.org/x/crypto/bcrypt"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -26,6 +29,101 @@ func clientHandler(w http.ResponseWriter, r *http.Request) {
 	// writeSampleJson(w)
 
 	http.ServeFile(w, r, title)
+}
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+	// Check the url extension for "signin", "signup", etc.
+	// Check if GET or POST request.
+
+	// fmt.Println(r.Method)
+
+	baseLocation := "api/users/"
+
+	location := r.URL.Path[1:]
+	location = strings.TrimPrefix(location, baseLocation)
+
+	fmt.Println("location", location)
+
+	switch location {
+
+	case "signup":
+		signup(w, r)
+
+	case "signin":
+		signin(w, r)
+
+	default:
+		fmt.Printf("No idea what this is: %v\n", location)
+	}
+}
+
+// type UserReq struct {
+// 	Username  string
+// 	Password  string
+// 	Firstname string
+// 	Lastname  string
+// }
+
+type UserRes struct {
+	Username  string
+	Password  string
+	Firstname string
+	Lastname  string
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Signup up...")
+
+	// Get JSON object.
+	decoder := json.NewDecoder(r.Body)
+	user := User{}
+	err := decoder.Decode(&user)
+	chkerr(err)
+	// fmt.Printf("%#v\n", user)
+	fmt.Printf("  %v\n", user.Username)
+
+	// Prepare to insert in the database.
+	user.Id = bson.NewObjectId()
+	user.CreatedAt = time.Now()
+
+	if user.Lastname == "" {
+		user.Lastname = "StoryTeller"
+	}
+
+	// Encrypt the password.
+	password := []byte(user.Password)
+	newPassword, err := bcrypt.GenerateFromPassword(password, 10)
+	chkerr(err)
+	user.Password = string(newPassword)
+
+	// Add the new user to the database.
+	collection := db.C("users")
+	err = collection.Insert(&user)
+	chkerr(err)
+
+	// Send back user information.
+	// js, err := json.Marshal(user)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// w.Header().Set("Content-Type", "application/json")
+	// w.Write(js)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Signed up!"))
+}
+
+func signin(w http.ResponseWriter, r *http.Request) {
+	// collection := db.C("testUsers")
+
+	fmt.Println("Signup in...")
+	decoder := json.NewDecoder(r.Body)
+	user := User{}
+	err := decoder.Decode(&user)
+	chkerr(err)
+	fmt.Printf("%#v\n", user)
 }
 
 func writeSampleJson(w http.ResponseWriter) {
