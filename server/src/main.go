ab11d09f5bdff85e9d70c726ecbfcc6c609db5f0
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-01-15 13:45:58 -0600

    Changed port environment variable from SERVER_PORT to PORT for Heroku

--- server/src/main.go
+++ server/src/main.go
@@ -28,7 +28,7 @@ func main() {
 	http.HandleFunc("/", clientHandler)
 	http.HandleFunc("/api/users/", usersHandler)
 
-	port := os.Getenv("SERVER_PORT")
+	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8020"
 	}
