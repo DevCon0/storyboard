124467f7adee4ed1282d471f3ffa5b9d67b0e8d5
David Milco <davidmilco@gmail.com>
2016-01-19 14:10:44 -0600

    Removing console logs
    Just getting rid of some console.logs in the singlestory.js file.

--- client/singleStory/singleStory.js
+++ client/singleStory/singleStory.js
@@ -2,10 +2,8 @@ angular.module('storyBoard.singleStory', [])
 
 .controller('singleStoryCtrl', function ($scope, StoryStorage, StoryStateMachine, $stateParams) {
   var storyID = $stateParams.storyId;
-  console.log('storyID', storyID);
   StoryStorage.getStory(storyID)
   .then(function (story) {
-    console.log('what do I have', story.data);
     StoryStateMachine.setStory(story.data);
   });
 });
