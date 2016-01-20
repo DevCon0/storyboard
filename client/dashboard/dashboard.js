15cd2b9e0c7c887eac2a62d1f725dc70fd32a0c9
David Milco <davidmilco@gmail.com>
2016-01-20 11:53:57 -0600

    Added Delete Functionality to Dashbord view/Controller/Factory

--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -8,6 +8,19 @@ angular.module('storyBoard.dashboard', [])
 
   $scope.username = localStorageService.get('username');
 
+  $scope.editStory = function (storyId) {
+    console.log('editStory function (controller) run with', storyId);
+
+  }
+
+  $scope.deleteStory = function (storyId) {
+    console.log('deleteStory function (controller) run with', storyId);
+    StoryStorage.deleteStory(storyId)
+    .then(function (resp) {
+      console.log('resp from deleteStory', resp);
+    })
+  }
+
   StoryStorage.getUserLibrary(localStorageService.get('sessiontoken'))
   .then(function(library){
     $scope.userLibrary = library;
