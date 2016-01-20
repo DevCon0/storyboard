10e6f8cf357014e5a4405b378f1fb967186c722b
John D <deblanc.john@gmail.com>
2016-01-20 13:31:18 -0600

    Starting the re-styling of the single story page

--- client/singleStory/singleStory.js
+++ client/singleStory/singleStory.js
@@ -1,9 +1,12 @@
 angular.module('storyBoard.singleStory', [])
 
 .controller('singleStoryCtrl', function ($scope, StoryStorage, StoryStateMachine, $stateParams) {
+  $scope.storyTitle = null;
+  $scope.storyAuthor = null;
   var storyID = $stateParams.storyId;
   StoryStorage.getStory(storyID)
   .then(function (story) {
+    $scope.storyTitle = story.data.title;
     StoryStateMachine.setStory(story.data);
   });
 });
