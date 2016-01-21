6576a4463aa67028753e7761a6ffe68da29f7a15
John D <deblanc.john@gmail.com>
2016-01-20 19:52:37 -0600

    Starting a facelift on the front end using polymer web components.

--- client/singleStory/singleStory.js
+++ client/singleStory/singleStory.js
@@ -2,11 +2,12 @@ angular.module('storyBoard.singleStory', [])
 
 .controller('singleStoryCtrl', function ($scope, StoryStorage, StoryStateMachine, $stateParams) {
   $scope.storyTitle = null;
-  $scope.storyAuthor = null;
+  $scope.storyUsername = null;
   var storyID = $stateParams.storyId;
   StoryStorage.getStory(storyID)
   .then(function (story) {
     $scope.storyTitle = story.data.title;
+    $scope.storyUsername = story.data.username;
     StoryStateMachine.setStory(story.data);
   });
 });
