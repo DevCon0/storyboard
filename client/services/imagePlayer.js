d504db0c72190b5c69493ba3e0912b810422194d
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-02-01 12:45:19 -0600

    After GIF's are done playing, stop their animation

--- client/services/imagePlayer.js
+++ client/services/imagePlayer.js
@@ -6,6 +6,8 @@ angular.module('storyBoard.imagePlayer', ['storyBoard.player', 'storyBoard.textT
     this.playerDiv = null;
     this.endPlaybackCallback = null;
     this.textToSpeechPlayer = null;
+    this.previewUrl = '';
+    this.imageUrl = '';
 
     this.narrationText = narrationText || '';
     var containsNarrationText = this.narrationText !== '';
@@ -18,9 +20,14 @@ angular.module('storyBoard.imagePlayer', ['storyBoard.player', 'storyBoard.textT
   ImagePlayer.prototype = Object.create(Player.prototype);
 
   ImagePlayer.prototype.create = function(storyFrame, readyCallback, endPlaybackCallback) {
+
     // Save image duration and end playback callback for later
     this.imageDuration = storyFrame.imageDuration;
     this.endPlaybackCallback = endPlaybackCallback;
+
+    // Save the urls used for the image tag's 'src' attribute.
+    this.previewUrl = storyFrame.previewUrl;
+    this.imageUrl = storyFrame.imageUrl;
 
     // Add image to DOM
     var divId = '#' + storyFrame.playerDiv;
@@ -49,15 +56,31 @@ angular.module('storyBoard.imagePlayer', ['storyBoard.player', 'storyBoard.textT
 
   ImagePlayer.prototype.play = function(){
     var imgTag = this.playerDiv.children();
+    
+    var imagePlayer = this;
+    
+    var imageUrl = imagePlayer.imageUrl;
+    var previewUrl = imagePlayer.previewUrl;
+
+    // Add the presentation url.
+    imgTag.attr('src', imageUrl);
+
     imgTag.addClass('showImagePlayerFrame');
     var durationInMilliseconds = this.imageDuration * 1000;
     var boundEndPlaybackCallback = this.endPlaybackCallback.bind(this);
 
     if (this.textToSpeechPlayer) {
-      this.textToSpeechPlayer.play();
+      this.textToSpeechPlayer.play();;
     }
 
     setTimeout(function(){
+      // If the image is a GIF,
+      //   reset it to the motionless PNG in the previewUrl.
+      var isAnimatedImage = (previewUrl !== imageUrl);
+      if (isAnimatedImage) {
+        imgTag.attr('src', previewUrl);
+      }
+
       boundEndPlaybackCallback();
     }, durationInMilliseconds);
   };
