bda975fc142186dc2523e08b05a50cd872bab44f
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-02-02 11:16:23 -0600

    Added Angular state to go to a user's profile

--- client/app.js
+++ client/app.js
@@ -75,6 +75,19 @@ storyBoardApp.config(function ($stateProvider, $urlRouterProvider) {
         }
       }
     })
+    .state('profile', {
+      url: '/profile/:username',
+      views: {
+        'navBar': {
+          templateUrl: '/navBar/navBar.html',
+          controller: 'navBarCtrl'
+        },
+        'content': {
+          templateUrl: '/dashboard/dashboard.html',
+          controller: 'dashboardCtrl'
+        }
+      }
+    })
     .state('singleStory', {
       url: '/singleStory/{storyId}',
       views: {
