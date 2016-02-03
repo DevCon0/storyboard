a7c9522b6f0d2dac79b1b1baf130458417f4856c
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-02-03 16:54:43 -0600

    Moving to a new page (url) kills text-to-speech players

--- client/services/textToSpeechPlayer.js
+++ client/services/textToSpeechPlayer.js
@@ -6,6 +6,7 @@ angular.module('storyBoard.textToSpeechPlayer', ['storyBoard.player'])
     this.playerDiv = null;
     this.utterance = null;
     this.narrationDelay = 0;
+    this.urlToPlayOn = '';
   }
 
   TextToSpeechPlayer.prototype = Object.create(Player.prototype);
@@ -38,6 +39,8 @@ angular.module('storyBoard.textToSpeechPlayer', ['storyBoard.player'])
   };
 
   TextToSpeechPlayer.prototype.play = function(){
+    this.urlToPlayOn = document.URL;
+
     if (this.narrationDelay) {
       setTimeout(this._play.bind(this), this.narrationDelay * 1000);
     } else {
@@ -50,7 +53,16 @@ angular.module('storyBoard.textToSpeechPlayer', ['storyBoard.player'])
       var paragraphTagStr = this.playerDiv.children();
       paragraphTagStr.addClass('showImagePlayerFrame');
     }
+
     window.speechSynthesis.speak(this.utterance);
+
+    var checkingIfNavigatedAway = setInterval(function() {
+      var haveNavigatedAway = (document.URL !== this.urlToPlayOn);
+      if (haveNavigatedAway) {
+        this.destroy();
+        clearInterval(checkingIfNavigatedAway);
+      }
+    }.bind(this), 100);
   };
 
   TextToSpeechPlayer.prototype._getBrowserSupportedVoice = function(){
