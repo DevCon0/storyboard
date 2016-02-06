ec20de08f3132718372570de407daee01a835724
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-02-06 17:27:24 -0600

    Navbar link labels are now constants

--- client/navBar/navBar.js
+++ client/navBar/navBar.js
@@ -8,6 +8,13 @@ angular.module('storyBoard.navBar', ['storyBoard.pageInfo'])
     $scope.currentUserLoggedIn = true;
   }
 
+  const TOP_STORIES_LINK = $scope.TOP_STORIES_LINK = 'Top Stories';
+  const CREATE_STORY_LINK = $scope.CREATE_STORY_LINK = 'Create Story';
+  const LIBRARY_LINK = $scope.LIBRARY_LINK = 'Library';
+  $scope.TOP_STORIES_LINK = TOP_STORIES_LINK;
+  $scope.CREATE_STORY_LINK = CREATE_STORY_LINK;
+  $scope.LIBRARY_LINK = LIBRARY_LINK;
+
   $scope.logout = function () {
     var token = localStorageService.get('sessiontoken');
     $scope.currentUserLoggedIn = false;
@@ -22,13 +29,13 @@ angular.module('storyBoard.navBar', ['storyBoard.pageInfo'])
     var endpoint = state.split('/')[1]
     switch (endpoint) {
     case '':
-      return 'Top Stories';
+      return TOP_STORIES_LINK;
       break;
     case 'createStory':
-      return 'Create Story';
+      return CREATE_STORY_LINK;
       break;
     case 'library':
-      return 'Library';
+      return LIBRARY_LINK;
       break;
     default:
       return PageInfo.get('title');
